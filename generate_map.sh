31a98bbf5f310d1874d4380e01128a5b0a413a5d
Yang <qikaiy2@golubh1.campuscluster.illinois.edu>
2019-09-23 01:04:36 -0500

    d

--- generate_map.sh
+++ generate_map.sh
@@ -1,19 +1,29 @@
-read_path="/projects/tallis/ekmolloy/multi-copy/data-old"
-store_path="/projects/tallis/ekmolloy/multi-copy/data-old"
+read_path="/projects/tallis/ekmolloy/multi-copy/data"
+store_path="/projects/tallis/ekmolloy/multi-copy/qikaiy2-data"
 
 cd $read_path
 
 read_files=$(ls -d */)
 for f in $read_files
 do
-	cd $read_path/$f
-	temp=$(ls -d */)
-	for sub_file in $temp
+	for i in {1..9}
 	do
-		cd $sub_file
-		echo 1
-		python /projects/tallis/ekmolloy/multi-copy/tools/generate_astral_map.py g_trees-raxml-sqlen-100.trees $store_path/$f/$sub_file/qikai_g_trees-raxml-sqlen-100_map.txt
-		cd ..
+	python /projects/tallis/ekmolloy/multi-copy/tools/generate_astral_map.py $read_path/$f/0$i/g_trees-raxml-sqlen-100-ngen-100.trees $store_path/$f/0$i/g_trees-raxml-sqlen-100-ngen-100_map.txt
+	python /projects/tallis/ekmolloy/multi-copy/tools/generate_astral_map.py $read_path/$f/0$i/g_trees-raxml-sqlen-100-ngen-25.trees  $store_path/$f/0$i/g_trees-raxml-sqlen-100-ngen-25_map.txt
+	python /projects/tallis/ekmolloy/multi-copy/tools/generate_astral_map.py $read_path/$f/0$i/g_trees-raxml-sqlen-100-ngen-500.trees $store_path/$f/0$i/g_trees-raxml-sqlen-100-ngen-500_map.txt
+
+	python /projects/tallis/ekmolloy/multi-copy/tools/generate_astral_map.py $read_path/$f/0$i/g_trees-raxml-sqlen-25-ngen-100.trees $store_path/$f/0$i/g_trees-raxml-sqlen-25-ngen-100_map.txt
+	python /projects/tallis/ekmolloy/multi-copy/tools/generate_astral_map.py $read_path/$f/0$i/g_trees-raxml-sqlen-25-ngen-25.trees $store_path/$f/0$i/g_trees-raxml-sqlen-25-ngen-25_map.txt
+	python /projects/tallis/ekmolloy/multi-copy/tools/generate_astral_map.py $read_path/$f/0$i/g_trees-raxml-sqlen-25-ngen-500.trees $store_path/$f/0$i/g_trees-raxml-sqlen-25-ngen-500_map.txt
+
+	python /projects/tallis/ekmolloy/multi-copy/tools/generate_astral_map.py $read_path/$f/0$i/g_trees-raxml-sqlen-250-ngen-100.trees  $store_path/$f/0$i/g_trees-raxml-sqlen-250-ngen-100_map.txt
+	python /projects/tallis/ekmolloy/multi-copy/tools/generate_astral_map.py $read_path/$f/0$i/g_trees-raxml-sqlen-250-ngen-25.trees $store_path/$f/0$i/g_trees-raxml-sqlen-250-ngen-25_map.txt
+	python /projects/tallis/ekmolloy/multi-copy/tools/generate_astral_map.py $read_path/$f/0$i/g_trees-raxml-sqlen-250-ngen-500.trees $store_path/$f/0$i/g_trees-raxml-sqlen-250-ngen-500_map.txt
+
+	python /projects/tallis/ekmolloy/multi-copy/tools/generate_astral_map.py $read_path/$f/0$i/g_trees-raxml-sqlen-50-ngen-100.trees $store_path/$f/0$i/g_trees-raxml-sqlen-50-ngen-100_map.txt
+	python /projects/tallis/ekmolloy/multi-copy/tools/generate_astral_map.py $read_path/$f/0$i/g_trees-raxml-sqlen-50-ngen-25.trees $store_path/$f/0$i/g_trees-raxml-sqlen-50-ngen-25_map.txt
+	python /projects/tallis/ekmolloy/multi-copy/tools/generate_astral_map.py $read_path/$f/0$i/g_trees-raxml-sqlen-50-ngen-500.trees $store_path/$f/0$i/g_trees-raxml-sqlen-50-ngen-500_map.txt
 	done
-	cd ..
+	
 done
+
